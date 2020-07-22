45228716e31295963415ce8b37344b71494380bf
Кислов Роман Сергеевич <r.kislov@PSO.LOCAL>
2020-07-22 13:09:04 +0500

    delete

--- routes/table.js
+++ routes/table.js
@@ -184,7 +184,7 @@ router.get('/:id/edit',auth,async (req,res) =>{
 
 })
 
-router.post('/remove/:id',auth, async (req,res)=>{
+router.get('/remove/:id',auth, async (req,res)=>{
     try {
         await Record.deleteOne({
             _id: req.body.id,
